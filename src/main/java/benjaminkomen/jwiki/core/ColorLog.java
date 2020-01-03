bc9bb9c9f24c65ca9152613149792a11436b5691
Benjamin Komen <benjamin.komen@duxxie.nl>
2020-01-03 13:20:51 +0100

    Fix logging.

--- src/main/java/benjaminkomen/jwiki/core/ColorLog.java
+++ src/main/java/benjaminkomen/jwiki/core/ColorLog.java
@@ -4,9 +4,6 @@ import lombok.Getter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 /**
  * Colors a String and logs it to console. Your terminal must support ASCII escapes for this to work, otherwise the text
  * will not be colored.
@@ -15,10 +12,6 @@ import java.time.format.DateTimeFormatter;
  */
 @Getter
 class ColorLog {
-    /**
-     * The date formatter prefixing output.
-     */
-    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("MMM dd, yyyy hh:mm:ss a");
 
     private static final Logger LOG = LoggerFactory.getLogger(ColorLog.class);
 
@@ -41,155 +34,52 @@ class ColorLog {
     }
 
     /**
-     * Logs a message for a wiki method.
+     * Output warning message for wiki.
      *
-     * @param wiki     The wiki object to use
-     * @param message  The String to print
-     * @param logLevel The identifier to log the message at (e.g. "INFO", "WARNING")
-     * @param color    The color to print the message with. Output will only be colored if this terminal supports it.
+     * @param wiki    The wiki object to use
+     * @param message The message to print.
      */
-    @SuppressWarnings("squid:S1192")
-    private void log(Wiki wiki, String message, LogLevel logLevel, Color color) {
-        if (enabled && (LOG.isDebugEnabled() || LOG.isErrorEnabled() || LOG.isInfoEnabled() || LOG.isTraceEnabled() || LOG.isWarnEnabled())) {
-            final String timestamp = LocalDateTime.now().format(DATE_TIME_FORMATTER);
-            switch (logLevel) {
-                case WARNING:
-                    LOG.warn(String.format("%s%n%s: \u001B[3%dm%s: %s\u001B[0m%n", timestamp, logLevel, color.colorValue, wiki, message));
-                    break;
-                case INFO:
-                    LOG.info(String.format("%s%n%s: \u001B[3%dm%s: %s\u001B[0m%n", timestamp, logLevel, color.colorValue, wiki, message));
-                    break;
-                case ERROR:
-                    LOG.error(String.format("%s%n%s: \u001B[3%dm%s: %s\u001B[0m%n", timestamp, logLevel, color.colorValue, wiki, message));
-                    break;
-                case DEBUG:
-                    LOG.debug(String.format("%s%n%s: \u001B[3%dm%s: %s\u001B[0m%n", timestamp, logLevel, color.colorValue, wiki, message));
-                    break;
-                case FYI:
-                    LOG.info(String.format("%s%n%s: \u001B[3%dm%s: %s\u001B[0m%n", timestamp, logLevel, color.colorValue, wiki, message));
-                    break;
-                default:
-                    throw new IllegalStateException("Log level " + logLevel + " is unknown.");
-            }
-        }
+    protected void warn(Wiki wiki, String message) {
+        LOG.warn("{} - {}", wiki, message);
     }
 
     /**
-     * Output warning message for wiki. Text is yellow.
+     * Output info message for wiki.
      *
-     * @param wiki The wiki object to use
-     * @param s    The String to print.
+     * @param wiki    The wiki object to use
+     * @param message The message to print.
      */
-    protected void warn(Wiki wiki, String s) {
-        log(wiki, s, LogLevel.WARNING, Color.YELLOW);
+    protected void info(Wiki wiki, String message) {
+        LOG.info("{} - {}", wiki, message);
     }
 
     /**
-     * Output info message for wiki. Text is green.
+     * Output error message for wiki.
      *
-     * @param wiki The wiki object to use
-     * @param s    The String to print.
+     * @param wiki    The wiki object to use
+     * @param message The message to print.
      */
-    protected void info(Wiki wiki, String s) {
-        log(wiki, s, LogLevel.INFO, Color.GREEN);
+    protected void error(Wiki wiki, String message) {
+        LOG.error("{} - {}", wiki, message);
     }
 
     /**
-     * Output error message for wiki. Text is red.
+     * Output debug message for wiki.
      *
-     * @param wiki The wiki object to use
-     * @param s    The String to print.
+     * @param wiki    The wiki object to use
+     * @param message The message to print.
      */
-    protected void error(Wiki wiki, String s) {
-        log(wiki, s, LogLevel.ERROR, Color.RED);
+    protected void debug(Wiki wiki, String message) {
+        LOG.debug("{} - {}", wiki, message);
     }
 
     /**
-     * Output debug message for wiki. Text is purple.
+     * Output miscellaneous message for wiki.
      *
-     * @param wiki The wiki object to use
-     * @param s    The String to print.
+     * @param wiki    The wiki object to use
+     * @param message The message to print.
      */
-    protected void debug(Wiki wiki, String s) {
-        log(wiki, s, LogLevel.DEBUG, Color.PURPLE);
-    }
-
-    /**
-     * Output miscellaneous message for wiki. Text is blue.
-     *
-     * @param wiki The wiki object to use
-     * @param s    The String to print.
-     */
-    protected void fyi(Wiki wiki, String s) {
-        log(wiki, s, LogLevel.FYI, Color.CYAN);
-    }
-
-    /**
-     * Represents ASCII colors.
-     *
-     * @author Fastily
-     */
-    private enum Color {
-        /**
-         * A font color, black, which can be applied to a String if your terminal supports it.
-         */
-        BLACK(0),
-
-        /**
-         * A font color, red, which can be applied to a String if your terminal supports it.
-         */
-        RED(1),
-
-        /**
-         * A font color, green, which can be applied to a String if your terminal supports it.
-         */
-        GREEN(2),
-
-        /**
-         * A font color, yellow, which can be applied to a String if your terminal supports it.
-         */
-        YELLOW(3),
-
-        /**
-         * A font color, blue, which can be applied to a String if your terminal supports it.
-         */
-        BLUE(4),
-
-        /**
-         * A font color, purple, which can be applied to a String if your terminal supports it.
-         */
-        PURPLE(5),
-
-        /**
-         * A font color, cyan, which can be applied to a String if your terminal supports it.
-         */
-        CYAN(6),
-
-        /**
-         * A font color, white, which can be applied to a String if your terminal supports it.
-         */
-        WHITE(7);
-
-        /**
-         * The ascii color value.
-         */
-        private final int colorValue;
-
-        /**
-         * Constructor, creates a new Color.
-         *
-         * @param colorValue The color code to use.
-         */
-        Color(int colorValue) {
-            this.colorValue = colorValue;
-        }
-    }
-
-    private enum LogLevel {
-        WARNING,
-        INFO,
-        ERROR,
-        DEBUG,
-        FYI;
+    protected void fyi(Wiki wiki, String message) {
+        LOG.trace("{} - {}", wiki, message);
     }
 }
